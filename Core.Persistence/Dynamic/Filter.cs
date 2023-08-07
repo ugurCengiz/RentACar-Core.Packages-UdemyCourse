259e40d03427d7c1a445ff274625339f636af24b
uğur cengiz <ugur_nobre@hotmail.com>
2023-08-07 18:50:46 +0300

    Add project files.

---
+++ Core.Persistence/Dynamic/Filter.cs
@@ -0,0 +1,24 @@
+﻿namespace Core.Persistence.Dynamic;
+
+public class Filter
+{
+    public string Field { get; set; }
+    public string? Value { get; set; }
+    public string Operator { get; set; }
+    public string? Logic { get; set; }
+
+    public IEnumerable<Filter> Filters { get; set; }
+
+    public Filter()
+    {
+        Field = String.Empty;
+        Operator = String.Empty;
+
+    }
+
+    public Filter(string field, string @operator)
+    {
+        field = Field;
+        Operator = @operator;
+    }
+}
