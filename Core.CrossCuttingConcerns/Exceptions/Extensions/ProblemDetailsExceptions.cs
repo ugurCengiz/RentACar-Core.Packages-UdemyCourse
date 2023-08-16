39d433c61ddb6d98f4714d323264b19fdacfec08
uğur cengiz <ugur_nobre@hotmail.com>
2023-08-16 18:49:45 +0300

    16.08.23 global error management Validation and Transaction implementation

---
+++ Core.CrossCuttingConcerns/Exceptions/Extensions/ProblemDetailsExceptions.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core.CrossCuttingConcerns.Exceptions.Extensions;
+
+public static class ProblemDetailsExceptions
+{
+    public static string AsJson<TProblemDetail>(this TProblemDetail details)
+        where TProblemDetail : ProblemDetails => JsonSerializer.Serialize(details);
+}
