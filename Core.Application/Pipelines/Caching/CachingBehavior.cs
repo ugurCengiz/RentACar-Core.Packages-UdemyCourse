754b256271bcb2be8d14305479949e40df5f7dee
uğur cengiz <ugur_nobre@hotmail.com>
2023-08-18 17:52:59 +0300

    Added InMemoryCache

---
+++ Core.Application/Pipelines/Caching/CachingBehavior.cs
@@ -0,0 +1,61 @@
+﻿using System.Text;
+using System.Text.Json;
+using MediatR;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Core.Application.Pipelines.Caching;
+
+public class CachingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>, ICachableRequest
+{
+    private readonly CacheSettings _cacheSettings;
+    private readonly IDistributedCache _cache;
+    private readonly ILogger<CachingBehavior<TRequest, TResponse>> _logger;
+
+
+    public CachingBehavior( IDistributedCache cache,IConfiguration configuration, ILogger<CachingBehavior<TRequest, TResponse>> logger)
+    {
+        _cacheSettings = configuration.GetSection("CacheSettings").Get<CacheSettings>() ?? throw new InvalidOperationException();
+        _cache = cache;
+        _logger = logger;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        if (request.BypassCache)
+        {
+            return await next();
+        }
+
+        TResponse response;
+        byte[]? cachedResponse = await _cache.GetAsync(request.CacheKey, cancellationToken);
+        if (cachedResponse != null)
+        {
+            response = JsonSerializer.Deserialize<TResponse>(Encoding.Default.GetString(cachedResponse));
+            _logger.LogInformation($"Fetched from cache -> {request.CacheKey}");
+        }
+        else
+        {
+            response = await getResponseAndAddToCache(request, next, cancellationToken);
+        }
+
+        return response;
+    }
+
+    private async Task<TResponse?> getResponseAndAddToCache(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        TResponse response = await next();
+
+        TimeSpan slidingExpiration = request.SlidingExpiration ?? TimeSpan.FromDays(_cacheSettings.SlidingExpiration);
+
+        DistributedCacheEntryOptions cacheOptions = new() { SlidingExpiration = slidingExpiration };
+
+        byte[] serializeData = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(response));
+
+        await _cache.SetAsync(request.CacheKey, serializeData, cacheOptions, cancellationToken);
+        _logger.LogInformation($"Added to Cache -> {request.CacheKey}");
+        return response;
+    }
+}
---
+++ Core.Application/Pipelines/Caching/ICachableRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Application.Pipelines.Caching;
+
+public interface ICachableRequest
+{
+    string CacheKey { get; }
+    bool BypassCache { get; }
+    TimeSpan? SlidingExpiration { get; }
+
+}
